ac2dc59ea1982128a3390268fec9f582acf30f13
Ketsama <Ketsama@DESKTOP-5E8T6IC>
2023-02-27 19:38:56 +0100

    Ajoutez des fichiers projet.

---
+++ OpenClassroom/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int age = 0;
+    float poids = 0;
+    int anneeNaissance = 0;
+
+    printf("Veuillez entrer votre age :\n");
+    scanf_s("%d", &age);
+
+    anneeNaissance = 2023 - age;
+
+    printf("Veuillez entrer votre poids :\n");
+    scanf_s("%f", &poids);
+    printf("Vous avez %d ans et vous faites %f kg\n", age, poids);
+    printf("Vous etes ne en %d", anneeNaissance);
+
+
+    return 0;
+}
