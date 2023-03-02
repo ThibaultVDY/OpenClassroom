e3f0e040536bf40dcefa8be031da63cb7278c672
Ketsama <Ketsama@DESKTOP-5E8T6IC>
2023-03-02 16:13:12 +0100

    ft_print_comb

--- OpenClassroom/main.c
+++ OpenClassroom/main.c
@@ -2,22 +2,33 @@
 #include <stdlib.h>
 #include <math.h>
 
+void ft_print_comb(void)
+{
+    char t[5];
+
+    t[0] = '0';
+    t[3] = ',';
+    t[4] = ' ';
+
+    while (t[0] <= '7') // quand la case 0 est plus petit ou égal à 7
+    {
+        t[1] = t[0] + 1; // on ajoute 1
+        while (t[1] <= '8') // quand la case 1 est plus petit ou égal à 8
+        {
+            t[2] = t[1] + 1; // on ajoute 1
+            while (t[2] <= '9') // quand la case 2 est plus petit ou égal à 9
+            {
+                write(1, t, 5); // fonction write 
+                t[2]++;
+            }
+            t[1]++;
+        }
+        t[0]++;
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    int age = 0;
-    float poids = 0;
-    int anneeNaissance = 0;
-
-    printf("Veuillez entrer votre age :\n");
-    scanf_s("%d", &age);
-
-    anneeNaissance = 2023 - age;
-
-    printf("Veuillez entrer votre poids :\n");
-    scanf_s("%f", &poids);
-    printf("Vous avez %d ans et vous faites %f kg\n", age, poids);
-    printf("Vous etes ne en %d", anneeNaissance);
-
-
+    ft_print_comb();
     return 0;
 }
