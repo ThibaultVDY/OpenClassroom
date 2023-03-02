50f4e41c6dc620d6b1612954aeab387afef5017f
Ketsama <Ketsama@DESKTOP-5E8T6IC>
2023-03-02 16:22:14 +0100

    comb 1.2

--- OpenClassroom/main.c
+++ OpenClassroom/main.c
@@ -18,7 +18,15 @@ void ft_print_comb(void)
             t[2] = t[1] + 1; // on ajoute 1
             while (t[2] <= '9') // quand la case 2 est plus petit ou égal à 9
             {
-                write(1, t, 5); // fonction write 
+                if (t[0] == '7' && t[1] == '8' && t[2] == '9') 
+                {
+                    write(1, t, 3); // write case 0 à 2
+                }
+                else 
+                {
+                    write(1, t, 5); // write case 0 à 4
+                }
+
                 t[2]++;
             }
             t[1]++;
